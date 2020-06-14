6e27bce67f92826a7ecc824d6ec2181ac1d15842
nolastemgarden <nolastemgarden@gmail.com>
2020-06-14 10:21:43 -0500

    Updates

---
+++ precache-manifest.1f522a2a888656ad4a24082013639576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532fd418fec30fb7420c86835ce42383",
+    "url": "/nolastemgarden.github.io/index.html"
+  },
+  {
+    "revision": "37fc0064ae03fb7684d7",
+    "url": "/nolastemgarden.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "07c2b924299af70113fc",
+    "url": "/nolastemgarden.github.io/static/js/2.5756a827.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nolastemgarden.github.io/static/js/2.5756a827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fc0064ae03fb7684d7",
+    "url": "/nolastemgarden.github.io/static/js/main.44fa5aa6.chunk.js"
+  },
+  {
+    "revision": "2d517670a45446f169da",
+    "url": "/nolastemgarden.github.io/static/js/runtime-main.94ecafd4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nolastemgarden.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75d69bfac2858814afde7d6f9ef5f72d.js"
+  "/nolastemgarden.github.io/precache-manifest.1f522a2a888656ad4a24082013639576.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nolastemgarden.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
