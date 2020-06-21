a8523f8377601d6113d4db3681fa624de81d608f
nolastemgarden <nolastemgarden@gmail.com>
2020-06-21 09:00:17 -0500

    Follow directions at https://levelup.gitconnected.com/deploying-your-react-website-to-github-pages-ac0e24601bdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
 
-
+//  MUI imports
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 // import CssBaseline from '@material-ui/core/CssBaseline';
 
 
-import {createMuiTheme, 
-        makeStyles, 
-        ThemeProvider } from '@material-ui/core/styles';
+import {
+    createMuiTheme, 
+    makeStyles, 
+    ThemeProvider } from '@material-ui/core/styles';
 
 import './App.css';
 
+import bananas_and_loquat from "./images/bananas_and_loquat.jpg";
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 import { Button, createStyles } from '@material-ui/core';
 
 const theme = createMuiTheme({
     palette: {
-        primary: purple,
+        primary: {
+            light: '#678b41',
+            main: '#3a5e16',
+            dark: '#123400',
+        },
         secondary: green,
     },
     status: {
@@ -30,14 +36,35 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles((theme) => ({
     app: {
-        backgroundColor: 'red',
+        
         minHeight: '100vh',
+        minWidth: '100vw',
 
     },
     box: {
-        minHeight: '50vh',
+        // minHeight: '50vh',
 
     },
+    heroImage: {
+        
+        backgroundImage: `
+            linear-gradient(180deg, rgba(58,94,22,0.70) 0%, rgba(58,94,22,0.00) 10%, rgba(58,94,22,0.00) 90%, rgba(58,94,22,0.70) 100%),
+            linear-gradient(90deg, rgba(58,94,22,0) 0%, rgba(58,94,22,0.80) 23%, rgba(58,94,22,0.80) 77%, rgba(58,94,22,0) 100%), 
+            url(${bananas_and_loquat})  
+        `,
+        // backgroundColor: 'rgb(58, 94, 22)',
+        width: 'min(100vw, 4032px)',
+        height: 'min(100vh, 75vw)',
+        overflowY: 'hidden',
+        // overflowX: 'hidden',
+        // minHeight: '520px',
+        backgroundPosition: 'left',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    },
+
+
+
 }));
 
 
@@ -52,19 +79,18 @@ export default function App() {
             <CssBaseline />
             <ThemeProvider theme={theme}>
                 
-                <Container 
+                <Box 
                     className={classes.app}
-                    maxWidth="lg"
-                    fixed={true}
+                    
                 >
                     <Box
-                        className={classes.box}
-                        maxWidth="lg"
-                        bgcolor="primary.light"
+                        className={classes.heroImage}
+                       
                     >
                     </Box>
                     
-                </Container>
+                    
+                </Box>
 
             </ThemeProvider>
         </React.Fragment>
