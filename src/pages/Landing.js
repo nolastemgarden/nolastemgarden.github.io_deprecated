1dc2afdc8089f1a19d0989379e9a77547c84c20e
nolastemgarden <nolastemgarden@gmail.com>
2020-06-22 20:12:01 -0500

    Mess with layout and Gradients over hero image

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+// MY components
+import LandingCards from "../components/LandingCards";
+
 // MY images
 import bananas_and_loquat from "../images/bananas_and_loquat.jpg";
+import logo from "../images/incomplete_logo.png";
 // import chessclub from "../../images/chessclub2.JPG";
 // import classroom from "../../images/classroom.JPG";
 
 // MUI Imports
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 
@@ -16,20 +21,28 @@ import Button from '@material-ui/core/Button';
 
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
-    box: {
-        // minHeight: '50vh',
+    root: {
+        minHeight: '100vh',
+        backgroundColor: theme.palette.primary.main,
+
 
     },
     heroImage: {
-
+        backgroundColor: 'rgb(58, 94, 22)',
         backgroundImage: `
-            linear-gradient(180deg, rgba(58,94,22,0.70) 0%, rgba(58,94,22,0.00) 10%, rgba(58,94,22,0.00) 90%, rgba(58,94,22,0.70) 100%),
-            linear-gradient(90deg, rgba(58,94,22,0) 0%, rgba(58,94,22,0.80) 23%, rgba(58,94,22,0.80) 77%, rgba(58,94,22,0) 100%), 
+            radial-gradient(circle, 
+                rgba(58,94,22,1.00) 30%, 
+                rgba(58,94,22,0.70) 50%,
+                rgba(58,94,22,0.00) 90%),
+            linear-gradient(0deg, 
+                rgba(58,94,22,0.95) 15%, 
+                rgba(58,94,22,0) 100%),
             url(${bananas_and_loquat})  
         `,
-        // backgroundColor: 'rgb(58, 94, 22)',
+        
         width: 'min(100vw, 4032px)',
         height: 'min(100vh, 75vw)',
+        maxHeight: '600px',
         overflowY: 'hidden',
         // overflowX: 'hidden',
         // minHeight: '520px',
@@ -37,6 +50,46 @@ const useStyles = makeStyles((theme) => ({
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
     },
+    logoArea: {
+        
+        // border: 'solid red 1px',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    logo: {
+        
+        height: '50%',
+        width: 'auto',
+    },
+    description: {
+        border: 'solid red 1px',
+        height: '10px',
+        width: '100%',
+        
+
+    },
+    cardArea: {
+        // border: 'solid red 1px',
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    pageBody: {
+        border: 'solid red 1px',
+        height: '100%',
+        width: '100%',
+
+        backgroundImage: `
+            
+        `,
+    }
 
 }));
 
@@ -45,15 +98,35 @@ export default function EnrichmentPage(props) {
 
     return (
         <Box
-            className={classes.app}
+            className={classes.root}
 
         >
             <Box
                 className={classes.heroImage}
 
             >
-            </Box>
+                <Container
+                    className={classes.logoArea}
+                    maxWidth='md'
+                >
+                    <img className={classes.logo} src={logo} alt="Logo" />
+                </Container>
+                <Container
+                    className={classes.description}
+                    maxWidth='md'
 
+                >
+                    {/* <img className={classes.logo} src={logo} alt="Logo" /> */}
+                </Container>
+            </Box>
+            <Container
+                className={classes.cardArea}
+                maxWidth='md'
+
+            >
+                <LandingCards />
+
+            </Container>
 
         </Box>
         
