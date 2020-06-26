574546fa1ba448a240a6302f80bb4d03bba8b5a5
nolastemgarden <nolastemgarden@gmail.com>
2020-06-26 15:53:16 -0500

    Copy bg imh and layout from OurStory page

--- src/pages/Teach.js
+++ src/pages/Teach.js
@@ -1,97 +1,106 @@
 import React from 'react';
 
-// MY components 
+// MY components
 import Navbar from "../components/Navbar";
+import LandingHeader from "../components/LandingHeader";
+import LandingCards from "../components/LandingCards";
 
 // MY images
-import bananas_and_loquat from "../images/bananas_and_loquat.jpg";
-// import chessclub from "../../images/chessclub2.JPG";
-// import classroom from "../../images/classroom.JPG";
+import bamboo from "../images/bamboo.jpg";
+import logo from "../images/incomplete_logo.png";
+import street_view_before from "../images/garden_before.jpg";
+
 
 // MUI Imports
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-
-import Button from '@material-ui/core/Button';
+import CardMedia from '@material-ui/core/CardMedia';
 
 
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Grid } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
     root: {
+        height: 'auto',
         minHeight: '100vh',
         backgroundColor: theme.palette.primary.main,
 
-
-    },
-    heroImage: {
-        backgroundColor: 'rgb(58, 94, 22)',
-        backgroundImage: `
-            radial-gradient(circle, 
-                rgba(58,94,22,0.95) 53%, 
-                rgba(58,94,22,0.00) 95%),
-            linear-gradient(180deg, 
-                rgba(58,94,22,1.00) 0%,    
-                rgba(58,94,22,0.9) 3%,             
-                rgba(58,94,22,0.00) 10%, 
-                rgba(58,94,22,0.00) 90%, 
-                rgba(58,94,22,1.00) 100%),
-            url(${bananas_and_loquat})  
-        `,
-
-        width: 'min(100vw, 4032px)',
-        height: 'min(100vh, 75vw)',
-        overflowY: 'hidden',
-        // overflowX: 'hidden',
-        // minHeight: '520px',
-        backgroundPosition: 'left',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-    },
-    pageBody: {
-        border: 'solid red 1px',
-        height: '100%',
         width: '100%',
 
+
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+
         backgroundImage: `
-            linear-gradient(90deg, 
-                rgba(58,94,22,0.00) 0%, 
-                rgba(58,94,22,0.90) 15%, 
-                rgba(58,94,22,0.90) 85%, 
-                rgba(58,94,22,0.00) 100%) 
+            
+            url(${bamboo})  
         `,
+        backgroundPosition: 'top',
+        backgroundRepeat: 'repeat',
+        backgroundSize: '40%',
+
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    body: {
+        // border: 'solid red 1px',
+        maxWidth: theme.breakpoints.values.md,
+        margin: 'min(10%, 50px)',
+        height: 'auto',
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        borderRadius: '1rem',
+        boxShadow: '0px 0px 5px 5px rgba(58,94,22,1)'
+
+    },
+    paragraph: {
+        textIndent: '2rem',
+        paddingBottom: '1rem',
+    },
+    cardArea: {
+        // border: 'solid red 1px',
+        height: 'auto',
+        width: '100%',
+
+
+    },
+    street_view_before: {
+        backgroundImage: `
+            url(${street_view_before})  
+        `,
+        backgroundSize: 'cover',
     }
 
 }));
 
-export default function TeachPage(props) {
+
+export default function GardenStoryPage(props) {
     const classes = useStyles();
 
     return (
-        <Box
-            className={classes.root}
+        <Box className={classes.root}  >
 
-        >
             <Navbar />
-        
-            <Box
-                className={classes.heroImage}
 
+            <Container
+                className={classes.body}
+                maxWidth='md'
             >
-                <Container
-                    className={classes.pageBody}
-                    maxWidth='md'
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography component='h1' variant='h2' gutterBottom >
+                            Teaching Services
+                        </Typography>
+                    </Grid>
 
-                >
 
-                </Container>
 
-            </Box>
-
+                </Grid>
+            </Container>
 
         </Box>
-
     )
-
 }
--- src/pages/WhatsGrowing.js
+++ src/pages/WhatsGrowing.js
@@ -6,28 +6,59 @@ import LandingHeader from "../components/LandingHeader";
 import LandingCards from "../components/LandingCards";
 
 // MY images
-import bananas_and_loquat from "../images/bananas_and_loquat.jpg";
+import bamboo from "../images/bamboo.jpg";
 import logo from "../images/incomplete_logo.png";
+import street_view_before from "../images/garden_before.jpg";
 
 
 // MUI Imports
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+
 
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
     root: {
         height: 'auto',
         minHeight: '100vh',
         backgroundColor: theme.palette.primary.main,
-        width: '100%'
-    },
-    landingHeader: {
 
         width: '100%',
-        height: '75vw',
+
+
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+
+        backgroundImage: `
+            
+            url(${bamboo})  
+        `,
+        backgroundPosition: 'top',
+        backgroundRepeat: 'repeat',
+        backgroundSize: '40%',
+
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    body: {
+        // border: 'solid red 1px',
+        maxWidth: theme.breakpoints.values.md,
+        margin: 'min(10%, 50px)',
+        height: 'auto',
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+        borderRadius: '1rem',
+        boxShadow: '0px 0px 5px 5px rgba(58,94,22,1)'
+
+    },
+    paragraph: {
+        textIndent: '2rem',
+        paddingBottom: '1rem',
     },
     cardArea: {
         // border: 'solid red 1px',
@@ -36,15 +67,54 @@ const useStyles = makeStyles((theme) => ({
 
 
     },
+    street_view_before: {
+        backgroundImage: `
+            url(${street_view_before})  
+        `,
+        backgroundSize: 'cover',
+    }
+
 }));
 
-export default function WhatsGrowingPage(props) {
+
+export default function GardenStoryPage(props) {
     const classes = useStyles();
 
     return (
         <Box className={classes.root}  >
-            
+
             <Navbar />
+
+            <Container
+                className={classes.body}
+                maxWidth='md'
+            >
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography component='h1' variant='h2' gutterBottom >
+                            What's Growing?
+                        </Typography>
+                    </Grid>
+                    
+
+
+
+
+                    <Grid item xs={4}>
+                        {/* <Card className={classes.root}>
+                            <CardMedia
+                                className={classes.media}
+                                image={street_view_before}
+                                title="Before Cleanup Picture"
+                            />
+                        </Card> */}
+                        <Box
+                            className={classes.street_view_before}
+                        />
+
+                    </Grid>
+                </Grid>
+            </Container>
 
         </Box>
     )
