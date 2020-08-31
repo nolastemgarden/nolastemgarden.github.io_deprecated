142f3923c900edc48a7b775b5e8a84eac79d10d9
nolastemgarden <nolastemgarden@gmail.com>
2020-08-31 16:34:36 -0500

    Update Landing.js

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -8,7 +8,7 @@ import Navbar from "../components/Navbar";
 // MY images
 import bananas_and_loquat from "../images/bananas_and_loquat.jpg";
 // import logo from "../images/incomplete_logo.png";
-
+import leaves from "../images/leaves.jpg";
 
 
 // MUI Imports
@@ -30,13 +30,38 @@ const useStyles = makeStyles((theme) => ({
     
 
     container: {
-        border: 'solid red 1px',
+        // border: 'solid red 1px',
         height: 'auto',
         width: '100%',
-        minHeight: '90vh',
-        backgroundColor: theme.palette.primary.main,
-        // backgroundColor: theme.palette.primary.light,
+        minHeight: '100vh',
+        // marginTop: '1rem',
+        // paddingTop: '1rem',
+        // backgroundColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.primary.light,
         // backgroundColor: theme.palette.common.white
+    },
+    leaves: {
+        border: 'solid  yellow 1px',
+        width: '100%',
+        paddingTop: '75%',
+
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
+
+        backgroundImage: `
+            linear-gradient(90deg, 
+                rgba(58,94,22,0.50) 0%, 
+                rgba(58,94,22,0.50) 100%),
+            url(${leaves})  
+        `,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     
 
@@ -61,14 +86,24 @@ export default function LandingPage(props) {
         <Box className={classes.root}  >
             <Navbar />
             
-            <Container className={classes.container}
+            <Container 
+                className={classes.container}
                 maxWidth='md' 
+                disableGutters
             >
-                <LandingHeader />
-                
-            
+                {/* <LandingHeader />
+                <LandingCards />  */}
 
-                <LandingCards /> 
+                <Box className={classes.leaves}  >
+                    {/* <Box className={classes.description}  >
+                        <Typography variant="h5" component="h2">
+                            Sowing the seeds of life-long learning and a DIY spirit through hands-on teaching
+                            of math, physics, plant biology, and computer programming.
+                        </Typography>
+
+                    </Box> */}
+
+                </Box>
             </Container>
             
 
